6a1e9409afe10ad647475067b81f2a369c1b70f2
Malik <malik.h@webdevpro.net>
2022-11-14 10:58:57 +0100

    start

---
+++ jour1/src/App.js
@@ -0,0 +1,37 @@
+/* import logo from './logo.svg';*/
+import './App.css'; 
+import React from "react"; 
+
+function App() {
+  return (
+    <div className="App">
+      {/*
+       * commentaire en jsx 
+       * binaire 0 ou 1 
+       * binaire => logiciel (logiciel / une application)
+       * logiciel visual 
+       * logiciel en ligne de commande 
+       * npm start => react-scripts start 
+       * cd 
+       * pwd 
+       * mv => logiciel en ligne de commande = bin
+       */}
+       {/**
+        * balise ouvrante balise fermante
+        */}
+        <h1>Obligatoire</h1>
+        {/** saut de ligne  il faut obligatoirement fermer la balise orpheline avec un / sur la balise ouvrante*/}
+         <br />
+         <img src="https://via.placeholder.com/400x200" alt="" className="border-10" /> 
+         <input type="text" />
+          <></>
+          {/* fragment react une zone */}
+          <React.Fragment></React.Fragment>
+          <br />
+          <label htmlFor="email">email</label> 
+          <input type="email" id="email" />
+    </div>
+  );
+}
+
+export default App;
