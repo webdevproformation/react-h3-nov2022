fe88ef20f68c22a4c24d92080026f78665f436ba
Malik <malik.h@webdevpro.net>
2022-11-15 09:40:42 +0100

    correction

--- jour2/src/App.js
+++ jour2/src/App.js
@@ -1,9 +1,16 @@
+import Exo1 from "./composants/Exo1";
 import Menu from "./composants/Menu";
 
 function App() {
   return (
     <div className="App">
         <Menu />
+        <main className="container">
+          <div className="row">
+            <Exo1 />
+          </div>
+        </main> 
+        
     </div>
   );
 }
---
+++ jour2/src/composants/Exo1.jsx
@@ -0,0 +1,23 @@
+const Exo1 = () => {
+    const articles = [
+        {id : 1 , titre :"article 1" , publie : true },
+        {id : 2 , titre :"article 2" , publie : false },
+        {id : 3 , titre :"article 3" , publie : false },
+        {id : 4 , titre :"article 4" , publie : true }
+    ]
+
+    return ( 
+        <div className="col-3">
+            {/** il faut obligatoirement avoir 1 balise parent comme racine du composant */}
+            <h1>Exo 1</h1>
+            <ul>
+                {articles.map((article , index) => {
+                    return (article.publie) && 
+                            <li key={article.id}>{article.titre}</li>
+                })}
+            </ul>
+        </div>
+     );
+}
+ 
+export default Exo1;
