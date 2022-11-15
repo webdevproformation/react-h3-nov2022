d359bf39cd83a62093a8711a8940a86c715a03fc
Malik <malik.h@webdevpro.net>
2022-11-15 10:27:35 +0100

    enoncé

--- jour2/src/App.js
+++ jour2/src/App.js
@@ -2,6 +2,7 @@ import Exo1 from "./composants/Exo1";
 import Menu from "./composants/Menu";
 import Event from "./composants/Event";
 import Exo2 from "./composants/Exo2";
+import State from "./composants/State";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Exo1 />
             <Event />
             <Exo2 />
+            <State />
           </div>
         </main> 
         
---
+++ jour2/src/composants/State.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const State = () => {
+    // const nb = 0 ;
+    // le state => variable locale au composant diaponible QUE dans le composant
+    // fonction lui donne la valeur par défaut de variable locale => 0
+    // retourne un tableau avec 2 valeurs 
+    // const [nb , setNb ] => destructuration du tableau
+    console.log(useState( 0 )); 
+    // nb 
+    // fonction qui permet de modifier la valeur 0 
+    const [nb , setNb ] = useState( 0 )
+    const augmenter = () => {
+        //const nb = document.querySelector(".badge").innerHTML 
+        //document.querySelector(".badge").innerHTML = parseInt(nb) + 1
+        // setNb(nb + 1); 
+        // le fait exécuter setNb() => modifier la valeur 0 => 1
+        // ET relancer le render / l'affichage de notre composant 
+        setNb( precedantNb =>   precedantNb + 1  )
+    }
+
+    return ( 
+        <div className="col-3 mt-3">
+            <button className="btn btn-warning" onClick={augmenter}>Augmenter</button>
+            <span className="badge bg-primary m-2">{nb}</span>
+        </div>
+     );
+}
+ 
+export default State;
