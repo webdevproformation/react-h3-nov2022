f9ceaba4a72a0a5520afdfec9c62c091a22a75d8
Malik <malik.h@webdevpro.net>
2022-11-15 09:22:00 +0100

    start jour2

---
+++ jour2/src/App.js
@@ -0,0 +1,11 @@
+import Menu from "./composants/Menu";
+
+function App() {
+  return (
+    <div className="App">
+        <Menu />
+    </div>
+  );
+}
+
+export default App;
---
+++ jour2/src/composants/Menu.jsx
@@ -0,0 +1,11 @@
+const Menu = () => {
+    return ( 
+       <div className="bg-primary">
+            <nav className="navbar navbar-expand navbar-light container">
+                <span className="navbar-brand">Jour 2 React</span>
+            </nav>
+       </div> 
+     );
+}
+ 
+export default Menu;
