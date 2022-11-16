c4e9fc338a56e1f6ed548207a9151624b68cea18
Malik <malik.h@webdevpro.net>
2022-11-16 15:00:37 +0100

    documentation

--- jour3/src/composants/Menu.jsx
+++ jour3/src/composants/Menu.jsx
@@ -1,6 +1,9 @@
 import { Link , NavLink } from "react-router-dom"
+import { useContext } from "react";
+import { UserContext } from "../contexts/useContext" // télécharger le store 
 
 const Menu = () => {
+    const { isLogged } = useContext(UserContext);
     return ( 
         <div className="bg-dark">
             <nav className="container navbar navbar-expand navbar-dark">
@@ -22,10 +25,24 @@ const Menu = () => {
                     </li>
                 </ul>
                 <ul className="navbar-nav ms-auto">
-                    <li className="nav-item">
-                        <NavLink to="/login" className={({ isActive }) =>
-                        isActive ? "nav-link active" : "nav-link"}>Login</NavLink>
-                    </li>
+                    {isLogged ?  
+                        <>
+                         <li className="nav-item">
+                            <NavLink to="/" className={({ isActive }) =>
+                            isActive ? "nav-link active" : "nav-link"}>Déconnexion</NavLink>
+                          </li>
+                          <li className="nav-item">
+                                <NavLink to="/dashboard" className={({ isActive }) =>
+                                isActive ? "nav-link active" : "nav-link"}>Dashboard</NavLink>
+                            </li>
+                        </>
+                    : 
+                        <li className="nav-item">
+                            <NavLink to="/login" className={({ isActive }) =>
+                            isActive ? "nav-link active" : "nav-link"}>Login</NavLink>
+                        </li>
+                    }
+                    
                 </ul>
             </nav>
         </div>
---
+++ jour3/src/contexts/useContext.js
@@ -0,0 +1,28 @@
+import { useState , createContext } from "react";
+
+export const UserContext = createContext() // store 
+
+// composant qui va emballer l'ensemble de notre application 
+// exactement comme <BrowserRouter> de la lib react router dom
+export const UserContextProvider = (props) => {
+    const [profil , setProfil] = useState({
+            login : "Alain" , 
+            password : "azerty" , 
+            isLogged : false
+    });
+
+    const connexion = (login , password) => {
+        if(login === profil.login && password === profil.password){
+            setProfil({...profil , isLogged : true})
+            return
+        }   
+        console.log("erreur lors de l'authentification")
+    }
+
+    return (
+        <UserContext.Provider value={{...profil , connexion}}>
+            {props.children}
+        </UserContext.Provider>
+    )
+}
+
