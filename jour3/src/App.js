ef04cd06febb0ddc1c01a8bdd630688803d96f77
Malik <malik.h@webdevpro.net>
2022-11-16 10:15:07 +0100

    système de routing

--- jour3/src/App.js
+++ jour3/src/App.js
@@ -1,11 +1,32 @@
 // npm i bootstrap 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Menu from "./composants/Menu";
+import { Routes , Route  } from "react-router-dom"
 
 function App() {
   return (
     <>
       <Menu />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<h1>bienvenu !!!</h1>} />
+          <Route path="/test" element={<h1>une page de test</h1>} />
+          <Route path="/contact" element={<h1>page contact</h1>} />
+          <Route path="/a-propos" element={<h1> A propos</h1>} />
+          {/**
+           * <Route path="/article" element={<h1>les articles</h1>} />
+            <Route path="/article/:id" element={<h1>un article</h1>} />
+           */}
+          <Route path="/article">
+            <Route index  element={<h1>les articles</h1>} />
+            <Route path=":id"  element={<h1>voir un article</h1>} />
+            <Route path="edit"  element={<h1>modifier article</h1>} />
+            <Route path="create"  element={<h1>créer article</h1>} />
+          </Route>
+          {/** route à mettre en dernier pour la gestion des urls introuvables */}
+          <Route path="*" element={<h1>erreur 404</h1>} />
+        </Routes>
+      </div>
     </>
   );
 }
