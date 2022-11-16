c04ff51a7cacbfabe10269e0465560e42de3805e
Malik <malik.h@webdevpro.net>
2022-11-16 11:39:49 +0100

    cas pratique

--- jour3/src/composants/front/Accueil.jsx
+++ jour3/src/composants/front/Accueil.jsx
@@ -2,19 +2,26 @@ import { useEffect , useState } from "react";
 
 const Accueil = () => {
     const [cocktails, setCocktails] = useState([])
+    const [recherche, setRecherche] = useState("vodka")
     useEffect( () => {
         async function getCocktails() {
-            const reponse = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=zorro");
+            const reponse = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+recherche);
             const {drinks} = await reponse.json();
             setCocktails(drinks);
         }
         getCocktails();
 
-    } , []);
+    } , [recherche]);
 
     return ( <div>
+        <div className="row my-3">
+            <input type="text" 
+                placeholder="rechercher" 
+                className="w-25" 
+                onChange={e => setRecherche(e.target.value)} />
+        </div>
         <div className="row">
-            { (cocktails.length > 0) ? 
+            { (cocktails && cocktails.length > 0) ? 
                  cocktails.map( cocktail  => {
                     return <div className="col-3" key={cocktail.idDrink}>
                         <h2>{cocktail.strDrink}</h2>
