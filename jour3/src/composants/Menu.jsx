4498776d88e71d1028adbb8306f90ce0ea8742f0
Malik <malik.h@webdevpro.net>
2022-11-16 10:24:19 +0100

    barre de menu

--- jour3/src/composants/Menu.jsx
+++ jour3/src/composants/Menu.jsx
@@ -1,3 +1,5 @@
+import { Link , NavLink } from "react-router-dom"
+
 const Menu = () => {
     return ( 
         <div className="bg-dark">
@@ -5,13 +7,18 @@ const Menu = () => {
                 <span className="navbar-brand"> Jour3 </span>
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a href="/" className="nav-link">Accueil</a>
+                        <NavLink to="/" className={({ isActive }) =>
+                        isActive ? "nav-link active" : "nav-link"}>Accueil</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a href="/a-propos" className="nav-link">A propos</a>
+                        <NavLink to="/a-propos" className={({ isActive }) =>
+                        isActive ? "nav-link active" : "nav-link"}>
+                            A propos
+                        </NavLink>
                     </li>
                     <li className="nav-item">
-                        <a href="/contact" className="nav-link">Contact</a>
+                        <NavLink to="/contact" className={({ isActive }) =>
+                        isActive ? "nav-link active" : "nav-link"}>Contact</NavLink>
                     </li>
                 </ul>
             </nav>
