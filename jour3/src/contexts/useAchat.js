f8d0c5dbeebfecc05f6aeca0edb61a75d62359d2
Malik <malik.h@webdevpro.net>
2022-11-16 16:53:05 +0100

    context

---
+++ jour3/src/contexts/useAchat.js
@@ -0,0 +1,17 @@
+import {createContext , useState} from "react";
+export const AchatContext = createContext();
+export const AchatProvider = (props) => {
+    const [panier, setPanier] = useState([]);
+
+    const addPanier = (produit) => {
+        const clonePanier = [...panier];
+        const existe = clonePanier.find((p) => { return p.id === produit.id })
+        if(!existe){
+            clonePanier.push(produit);
+            setPanier(clonePanier)
+        }
+    }
+    return <AchatContext.Provider value={{panier , addPanier}}>
+        {props.children}
+    </AchatContext.Provider>
+}
